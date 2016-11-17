c1a8f7599b927837754a43e133bd862742d6dbee
gmadges <gmadgesdeveloper@gmail.com>
2016-11-17 22:09:59 +0000

    not doing any distance checks atm

--- src/physicsModel.cpp
+++ src/physicsModel.cpp
@@ -112,13 +112,13 @@ void PhysicsModel::applyConstraints()
             auto compareBody = rigid_bodies[j].first;
             btVector3 comparePos = getPositionForBody(compareBody);
 
-            // //check if their near each other
-            // float dist = bodyPos.dot(comparePos);
-            // if(abs(dist) <= bodyRadius)
-            // {
+            //check if their near each other
+            //float dist = bodyPos.dot(comparePos);
+            //if(abs(dist) <= bodyRadius)
+            //{
                 addConstraint(body, compareBody);
                 consts[i].push_back(j);
-            // } 
+            //} 
         }
     }
 }
@@ -127,25 +127,12 @@ void PhysicsModel::addConstraint(std::shared_ptr<btRigidBody> pBody1, std::share
 {
     // get positions
     btVector3 rigid1Pos = getPositionForBody(pBody1);
-    btVector3 rigid2Pos = getPositionForBody(pBody2);
 
     btTransform frameInA;
     btTransform frameInB;
     
     frameInA.setIdentity();
     frameInB.setIdentity();
-
-    // btVector3 axis1(axis1X,axis1Y,axis1Z);
-    // btVector3 axis2(axis2X,axis2Y,axis2Z);
-
-    // if (axis1.length() == 0.0)
-    // {
-    //     btPlaneSpace1( axisInA, axis1, axis2 );
-    // }
-
-    // frameInA.getBasis().setValue( axisInA.x(), axis1.x(), axis2.x(),
-    //                                 axisInA.y(), axis1.y(), axis2.y(),
-    //                                 axisInA.z(), axis1.z(), axis2.z() );
     
     frameInA.setOrigin( rigid1Pos );
 
@@ -156,9 +143,9 @@ void PhysicsModel::addConstraint(std::shared_ptr<btRigidBody> pBody1, std::share
     frameInB = inv  * globalFrameA;
 
     btFixedConstraint *constraint = new btFixedConstraint(*(pBody1.get()),
-                                                                        *(pBody2.get()),
-                                                                        frameInA,
-                                                                        frameInB);
+                                                            *(pBody2.get()),
+                                                            frameInA,
+                                                            frameInB);
 
     pPhysicsWorld->addConstraint(constraint);
 
