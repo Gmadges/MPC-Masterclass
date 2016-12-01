c31ac1e1661134c365fd6ca4e420a8e4882fc502
gmadges <gmadgesdeveloper@gmail.com>
2016-12-01 23:37:37 +0000

    added a pure virtual function to initUI

--- include/settings.h
+++ include/settings.h
@@ -15,6 +15,12 @@ public:
     Settings(QWidget *parent = 0);
     virtual ~Settings();
 
+    // TODO abstract method that returns settings
+    //virtual void getSettings() = 0;
+    
+private:
+    virtual void initUI() = 0;
+
 public:
     Ui::Settings *ui;
 };
